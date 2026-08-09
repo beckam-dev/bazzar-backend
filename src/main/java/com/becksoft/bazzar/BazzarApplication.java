3d997675ea6819a28a18247ab54c66b5eadf0416
beckam-dev <beckamDev@hotmail.com>
2026-08-09 18:29:24 -0500

    base

---
+++ src/main/java/com/becksoft/bazzar/BazzarApplication.java
@@ -0,0 +1,13 @@
+package com.becksoft.bazzar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazzarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazzarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/becksoft/bazzar/BazzarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.becksoft.bazzar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazzarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
